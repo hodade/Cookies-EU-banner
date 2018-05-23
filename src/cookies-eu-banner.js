1b48818d5693b3590db98407ea87498eff4d0716
tanaka <fj8353ae@aa.jp.fujitsu.com>
2018-05-23 20:34:05 +0900

    customize for AreaInfo

--- src/cookies-eu-banner.js
+++ src/cookies-eu-banner.js
@@ -1,163 +1,192 @@
+/**
+ * Cookies EU banner v1.2.10 - Manage display of banner to accept/reject cookies from tracking services like Google Analytics
+ * ------------------------
+ * @link http://alex-d.github.io/Cookies-EU-banner/
+ * @license MIT
+ * @author Alex-D
+ *         Twitter : @AlexandreDemode
+ *         Website : alex-d.fr
+ */
+
 ; // jshint ignore:line
 (function (root, factory, undefined) {
-  'use strict';
-  if (typeof define === 'function' && define.amd) {
-    define([], factory);
-  } else if (typeof exports === 'object') {
-    module.exports = factory();
-  } else {
-    // root is window
-    root.CookiesEuBanner = factory();
-  }
+    'use strict';
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        // root is window
+        root.CookiesEuBanner = factory();
+    }
 }(window, function () {
-  'use strict';
+    'use strict';
 
-  var CookiesEuBanner,
-    document = window.document;
+    var CookiesEuBanner,
+        document = window.document;
 
-  CookiesEuBanner = function (launchFunction, waitAccept, undefined) {
-    if (!(this instanceof CookiesEuBanner)) {
-      return new CookiesEuBanner(launchFunction);
-    }
+    CookiesEuBanner = function () {
+        if (!(this instanceof CookiesEuBanner)) {
+            console.log('instanceof');
+            return new CookiesEuBanner();
+        }
 
-    this.cookieTimeout = 33696000000; // 13 months in milliseconds
-    this.bots = /bot|googlebot|crawler|spider|robot|crawling/i;
-    this.cookieName = 'hasConsent';
-    this.trackingCookiesNames = [ '__utma', '__utmb', '__utmc', '__utmt', '__utmv', '__utmz', '_ga', '_gat', '_gid' ];
-    this.launchFunction = launchFunction;
-    this.waitAccept = waitAccept || false;
-    this.init();
-  };
+        this.cookieTimeout = 33696000000; // 13 months in milliseconds
+        this.bots = /bot|googlebot|crawler|spider|robot|crawling/i;
+        this.cookieName = 'hasConsent';
+        this.trackingCookiesNames = [ '__utma', '__utmb', '__utmc', '__utmt', '__utmv', '__utmz', '_ga', '_gat', 'PHPSESSID' ];
+        this.waitAccept = true;
+        this.init();
+    };
 
-  CookiesEuBanner.prototype = {
-    init: function () {
-      // Do nothing if it is a bot
-      // If DoNotTrack is activated, do nothing too
-      if (this.isBot() || !this.isToTrack() || this.hasConsent() === false) {
-        return false;
-      }
+    CookiesEuBanner.prototype = {
+        init: function () {
+            // Do nothing if it is a bot
+            // If DoNotTrack is activated, do nothing too
+            if (this.isBot() || !this.isToTrack()) {
+                return false;
+            }
 
-      // User has already consent to use cookies to tracking
-      if (this.hasConsent() === true) {
-        // Launch user custom function
-        this.launchFunction();
-        return true;
-      }
+            // // User has already consent to use cookies to tracking
+            // if (this.hasConsent() === true) {
+            //   // Launch user custom function
+            //   this.launchFunction();
+            //   return true;
+            // }
 
-      // If it's not a bot, no DoNotTrack and not already accept : show banner
-      this.showBanner();
+            // If it's not a bot, no DoNotTrack and not already accept : show banner
+            this.showBanner();
 
-      if (!this.waitAccept) {
-        // Accept cookies by default for the next page
-        this.setCookie(this.cookieName, true);
-      }
-    },
+            if (!this.waitAccept) {
+                // Accept cookies by default for the next page
+                this.setCookie(this.cookieName, true);
+            }
+        },
 
-    /*
-     * Show banner at the top of the page
-     */
-    showBanner: function () {
-      var _this = this,
-        banner = document.getElementById('cookies-eu-banner'),
-        rejectButton = document.getElementById('cookies-eu-reject'),
-        acceptButton = document.getElementById('cookies-eu-accept'),
-        moreLink = document.getElementById('cookies-eu-more');
+        setLaunchFunction: function (f) {
+            this.launchFunction = f;
+        },
 
-      banner.style.display = 'block';
+        setLaunchFunctionReject: function (f) {
+            this.launchFunctionReject = f;
+        },
 
-      if (moreLink) {
-        this.addEventListener(moreLink, 'click', function () {
-          _this.deleteCookie(_this.cookieName);
-        });
-      }
+        /*
+         * Show banner at the top of the page
+         */
+        showBanner: function () {
+            var _this = this,
+                banner = document.getElementById('cookies-eu-banner'),
+                cover = document.getElementById('cookies-eu-cover'),
+                rejectButton = document.getElementById('cookies-eu-reject'),
+                acceptButton = document.getElementById('cookies-eu-accept'),
+                moreLink = document.getElementById('cookies-eu-more');
 
-      if (acceptButton) {
-        this.addEventListener(acceptButton, 'click', function () {
-          banner.parentNode.removeChild(banner);
-          _this.setCookie(_this.cookieName, true);
-          _this.launchFunction();
-        });
-      }
+            banner.style.display = 'block';
+            cover.style.display = 'block';
 
-      if (rejectButton) {
-        this.addEventListener(rejectButton, 'click', function () {
-          banner.parentNode.removeChild(banner);
-          _this.setCookie(_this.cookieName, false);
-          _this.deleteTrackingCookies();
-        });
-      }
-    },
+            if (moreLink) {
+                this.addEventListener(moreLink, 'click', function () {
+                    _this.deleteCookie(_this.cookieName);
+                });
+            }
 
-    /*
-     * Check if user already consent
-     */
-    hasConsent: function () {
-      if (document.cookie.indexOf(this.cookieName + '=true') > -1) {
-        return true;
-      } else if (document.cookie.indexOf(this.cookieName + '=false') > -1) {
-        return false;
-      }
+            if (acceptButton) {
+                this.addEventListener(acceptButton, 'click', function () {
+                    _this.hideBanner();
+                    _this.setCookie(_this.cookieName, true);
+                    _this.launchFunction();
+                });
+            }
 
-      return null;
-    },
+            if (rejectButton) {
+                this.addEventListener(rejectButton, 'click', function () {
+                    _this.hideBanner();
+                    _this.setCookie(_this.cookieName, false);
+                    _this.deleteTrackingCookies();
+                    _this.launchFunctionReject();
+                });
+            }
+        },
 
-    /*
-     * Detect if the visitor is a bot or not
-     * Prevent for search engine take the cookie
-     * alert message as main content of the page
-     */
-    isBot: function () {
-      return this.bots.test(navigator.userAgent);
-    },
+        hideBanner: function() {
+            var banner = document.getElementById('cookies-eu-banner'),
+                cover = document.getElementById('cookies-eu-cover');
 
-    /*
-     * Check if DoNotTrack is activated
-     */
-    isToTrack: function () {
-      var dnt = navigator.doNotTrack || navigator.msDoNotTrack || window.doNotTrack;
-      return (dnt !== null && dnt !== undefined) ? (dnt && dnt !== 'yes' && dnt !== 1 && dnt !== '1') : true;
-    },
+            banner.style.display = 'none';
+            cover.style.display = 'none';
+        },
 
-    /*
-     * Delete existent tracking cookies
-     */
-    deleteTrackingCookies: function () {
-      var _this = this;
-      this.trackingCookiesNames.map(function (cookieName) {
-        _this.deleteCookie(cookieName);
-      });
-    },
+        /*
+         * Check if user already consent
+         */
+        hasConsent: function () {
+            if (document.cookie.indexOf(this.cookieName + '=true') > -1) {
+                return true;
+            } else if (document.cookie.indexOf(this.cookieName + '=false') > -1) {
+                return false;
+            }
 
-    /*
-     * Create/update cookie
-     */
-    setCookie: function (name, value) {
-      var date = new Date();
-      date.setTime(date.getTime() + this.cookieTimeout);
+            return null;
+        },
 
-      document.cookie = name + '=' + value + ';expires=' + date.toGMTString() + ';path=/';
-    },
+        /*
+         * Detect if the visitor is a bot or not
+         * Prevent for search engine take the cookie
+         * alert message as main content of the page
+         */
+        isBot: function () {
+            return this.bots.test(navigator.userAgent);
+        },
 
-    /*
-     * Delete cookie by changing expire
-     */
-    deleteCookie: function (name) {
-      var hostname = document.location.hostname;
-      if (hostname.indexOf('www.') === 0) {
-        hostname = hostname.substring(4);
-      }
-      document.cookie = name + '=; domain=.' + hostname + '; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/';
-      document.cookie = name + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/';
-    },
+        /*
+         * Check if DoNotTrack is activated
+         */
+        isToTrack: function () {
+            var dnt = navigator.doNotTrack || navigator.msDoNotTrack || window.doNotTrack;
+            return (dnt !== null && dnt !== undefined) ? (dnt && dnt !== 'yes' && dnt !== 1 && dnt !== '1') : true;
+        },
 
-    addEventListener: function (DOMElement, evnt, callback) {
-      if (document.addEventListener) { // For all major browsers, except IE 8 and earlier
-        DOMElement.addEventListener(evnt, callback);
-      } else if (DOMElement.attachEvent) { // For IE 8 and earlier versions
-        DOMElement.attachEvent('on' + evnt, callback);
-      }
-    }
-  };
+        /*
+         * Delete existent tracking cookies
+         */
+        deleteTrackingCookies: function () {
+            var _this = this;
+            this.trackingCookiesNames.map(function (cookieName) {
+                _this.deleteCookie(cookieName);
+            });
+        },
 
-  return CookiesEuBanner;
+        /*
+         * Create/update cookie
+         */
+        setCookie: function (name, value) {
+            var date = new Date();
+            date.setTime(date.getTime() + this.cookieTimeout);
+
+            document.cookie = name + '=' + value + ';expires=' + date.toGMTString() + ';path=/';
+        },
+
+        /*
+         * Delete cookie by changing expire
+         */
+        deleteCookie: function (name) {
+            var hostname = document.location.hostname;
+            if (hostname.indexOf('www.') === 0) {
+                hostname = hostname.substring(4);
+            }
+            document.cookie = name + '=; domain=.' + hostname + '; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/';
+            document.cookie = name + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/';
+        },
+
+        addEventListener: function (DOMElement, evnt, callback) {
+            if (document.addEventListener) { // For all major browsers, except IE 8 and earlier
+                DOMElement.addEventListener(evnt, callback);
+            } else if (DOMElement.attachEvent) { // For IE 8 and earlier versions
+                DOMElement.attachEvent('on' + evnt, callback);
+            }
+        }
+    };
+
+    return CookiesEuBanner;
 }));
